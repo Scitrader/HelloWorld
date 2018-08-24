de5e57d2e4cfcbf28cd11e006b34f51f25ef5188
birdTee <birdonbox@gmail.com>
2018-08-24 16:54:21 +0700

    Finish 24/8/2561

---
+++ app/src/main/java/teerapongz/androidthai/in/th/birdhello/RegisterFragment.java
@@ -0,0 +1,19 @@
+package teerapongz.androidthai.in.th.birdhello;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register,container,false);
+        return view;
+    }
+}
